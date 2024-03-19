77b5f0c5c0dd46ed22b5147cdb6d792c7cff7f4b
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-19 19:40:57 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+   if (ride <= 400) {
+    return 'This one is on me!';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+   } else if (ride <= 2000) {
+    return 'That will be twenty bucks.';
+
+   } else if (ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+
+   } else {
+     return 'No can do.';
+   }
 }
+scuberGreetingForFeet();
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return  (city === 'NYC')? ("Ok, sounds good.") : ("No go.");
+}
+ternaryCheckCity();
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+    default:
+  }
+
+}
+switchOnCharmFromTip();
